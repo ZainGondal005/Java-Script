b2edd690b8846641d9148c58f912f014e09b2b38
ZainGondal005 <zaingondal005@gmail.com>
2024-02-06 23:28:52 +0500

    Data types

--- js_Fundamentls_part_1/script.js
+++ js_Fundamentls_part_1/script.js
@@ -1,4 +1,20 @@
 let js = "zainMasood";
 if (js == "zainMasood") {
-  alert("javascript is fun");
+  // alert("javascript is fun");
+  console.log(js);
 }
+
+let seetype = "zain"; //any data type change into other data type
+
+console.log(typeof seetype);
+seetype = true;
+console.log(typeof seetype);
+
+let year;//value not assigned are called undefined
+year=1992;
+console.log(typeof year);
+
+year=null; // null is considered as object
+console.log(typeof year);
+
+
