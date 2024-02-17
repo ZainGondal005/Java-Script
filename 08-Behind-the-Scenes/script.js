7a57062edf2bafde427478b406016bb99af47c3c
ZainGondal005 <zaingondal005@gmail.com>
2024-02-17 23:01:32 +0500

    Primitives and Objects

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -123,7 +123,7 @@ console.log(z === window.z);
 
 ///////////////////////////////////////
 // Regular Functions vs. Arrow Functions
-var firstName = 'Matilda';
+var firstName = "Matilda";
 
 const jonas = {
   firstName: "Jonas",
@@ -191,39 +191,36 @@ console.log('Me', me);
 // Primitives vs. Objects in Practice
 
 // Primitive types
-let lastName = 'Williams';
+let lastName = "Williams";
 let oldLastName = lastName;
-lastName = 'Davis';
+lastName = "Davis";
 console.log(lastName, oldLastName);
 
 // Reference types
 const jessica = {
-  firstName: 'Jessica',
-  lastName: 'Williams',
+  firstName: "Jessica",
+  lastName: "Williams",
   age: 27,
 };
 const marriedJessica = jessica;
-marriedJessica.lastName = 'Davis';
-console.log('Before marriage:', jessica);
-console.log('After marriage: ', marriedJessica);
+marriedJessica.lastName = "Davis";
+console.log("Before marriage:", jessica);
+console.log("After marriage: ", marriedJessica);
 // marriedJessica = {};
 
 // Copying objects
 const jessica2 = {
-  firstName: 'Jessica',
-  lastName: 'Williams',
+  firstName: "Jessica",
+  lastName: "Williams",
   age: 27,
-  family: ['Alice', 'Bob'],
+  family: ["Alice", "Bob"],
 };
 
 const jessicaCopy = Object.assign({}, jessica2);
-jessicaCopy.lastName = 'Davis';
+jessicaCopy.lastName = "Davis";
 
-jessicaCopy.family.push('Mary');
-jessicaCopy.family.push('John');
+jessicaCopy.family.push("Mary");
+jessicaCopy.family.push("John");
 
-console.log('Before marriage:', jessica2);
-console.log('After marriage: ', jessicaCopy);
-
-
-
+console.log("Before marriage:", jessica2);
+console.log("After marriage: ", jessicaCopy);
