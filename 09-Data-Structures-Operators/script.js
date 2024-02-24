13fde5308bf5a834c5acdecc593af0138bb208a7
ZainGondal005 <zaingondal005@gmail.com>
2024-02-23 16:23:41 -0800

    Map

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -358,35 +358,68 @@ for (const [team, odd] of Object.entries(football.odds)) {
 
 // Sets
 const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
 ]);
 console.log(ordersSet);
 
-console.log(new Set('Jonas'));
+console.log(new Set("Jonas"));
 
 console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-console.log(ordersSet.has('Bread'));
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-ordersSet.delete('Risotto');
+console.log(ordersSet.has("Pizza"));
+console.log(ordersSet.has("Bread"));
+ordersSet.add("Garlic Bread");
+ordersSet.add("Garlic Bread");
+ordersSet.delete("Risotto");
 // ordersSet.clear();
 console.log(ordersSet);
 
 for (const order of ordersSet) console.log(order);
 
 // Example
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
 const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
 
 console.log(
-  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+  new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
 );
 
-console.log(new Set('zainmasood').size);
+console.log(new Set("zainmasood").size);
+
+// Map
+
+const rest = new Map();
+rest.set("name", "classico");
+rest.set(1, "france");
+rest.set(2, "Italy");
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open :D")
+  .set(false, "We are closed :(");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
+// rest.clear();
+
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+// console.log(rest.get(arr));
