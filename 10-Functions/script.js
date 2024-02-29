2fc93d847c15e2539d456aebb182cdbcc945fe77
ZainGondal005 <zaingondal005@gmail.com>
2024-02-29 23:53:27 +0500

    Higher-order function

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -29,9 +29,9 @@ const checkIn = function (flightNum, passenger) {
   passenger.name = 'Mr. ' + passenger.name;
 
   if (passenger.passport === 24739479284) {
-    alert('Checked in');
+    // alert('Checked in');
   } else {
-    alert('Wrong passport!');
+    // alert('Wrong passport!');
   }
 };
 
@@ -49,3 +49,25 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+ 
