ee5f8af8413d49c3f2af4b0e64bbd2e770cf2c03
ZainGondal005 <zaingondal005@gmail.com>
2024-03-13 01:32:43 +0500

    insert delete in HTML

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -1,31 +1,54 @@
-'use strict';
+"use strict";
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
+});
+
+//Lecture
+const header = document.querySelector(".header");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+console.log(document.getElementsByClassName("btn"));
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = "we used cookie for improved verssion";
+message.innerHTML =
+  "we used cookie for improved versions and functionality.<button class='btn btn--close-cookie'>Got it!</button>";
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true))
+
+// header.before(message);
+header.after(message);
+
+document.querySelector(".btn--close-cookie");
+addEventListener("click", function () {
+  message.remove();
 });
