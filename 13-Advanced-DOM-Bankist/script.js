94622a9068ac6de9410c45460baf53e9c38a84af
ZainGondal005 <zaingondal005@gmail.com>
2024-03-14 07:01:08 +0500

    add scroll yo learn more button

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -29,6 +29,31 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function () {
+  const s1coords = section1.getBoundingClientRect();
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  //old
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
 //Lecture
 const header = document.querySelector(".header");
 const allButtons = document.getElementsByTagName("button");
