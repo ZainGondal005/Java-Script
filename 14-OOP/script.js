5d9e87d83cd6229f0a31521c5ea2dcc8700809eb
ZainGondal005 <zaingondal005@gmail.com>
2024-03-23 05:18:06 +0500

    Prototypes,Challenge #1,classses getter and setter static fnctions

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,147 @@
-'use strict';
+"use strict";
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person("jonas", 1991);
+console.log(jonas);
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+
+console.log(jonas instanceof Person);
+
+//   Person.hey = function () {
+//     console.log('Hey there 👋');
+//     console.log(this);
+//   };
+//   Person.hey();
+
+///////////////////////////////////////
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty("firstName"));
+console.log(jonas.hasOwnProperty("species"));
+
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir((x) => x + 1);
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+const bmw = new Car("BMWs", 120);
+const mercedes = new Car("Mercedes", 95);
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+bmw.break();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+
+class PersonCl {
+  constructor(firstName, age) {
+    this.firstName = firstName;
+    this.age = age;
+  }
+  calcAge() {
+    console.log(2024 - this.age);
+  }
+  get agee() {
+    return 2024 - this.age;
+  } // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+  // Static method
+  static hey() {
+    console.log("Hey there 👋");
+    console.log(this);
+  }
+}
+const Person1 = new PersonCl("zain masood", 20);
+
+console.log(Person1);
+Person1.calcAge();
+console.log(Person1.agee);
+const walter = new PersonCl("Walter White", 1965);
+const account = {
+  owner: "zain",
+  movements: [200, 400, -699, 800],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
