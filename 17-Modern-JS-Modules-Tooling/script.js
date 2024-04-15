4b2424e223afa9a7b41c07742c082107c6b35d55
ZainGondal005 <zaingondal005@gmail.com>
2024-04-15 22:12:23 +0500

    ShoppingCart2 use for module pattern

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -31,3 +31,32 @@ const getLastPost = async function () {
 getLastPost();
 const lastPost = await getLastPost();
 console.log(lastPost);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+ShoppingCart2.addToCart("apple", 4);
+ShoppingCart2.addToCart("pizza", 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
