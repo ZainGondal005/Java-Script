d7fce160148fa7fb3ddaa1da276566ef8ee1da75
ZainGondal005 <zaingondal005@gmail.com>
2024-04-18 22:31:49 +0500

    install parcel and fetch api and design object

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcebc"
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${data.status})`);
+
+    console.log(res, "data is", data);
+
+    let { recipe } = data.data;
+    console.log(recipe);
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+console.log("TESAT");
+showRecipe();
